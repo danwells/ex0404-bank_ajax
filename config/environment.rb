41591613564fcf6f2962a074096c52cd7ceadb53
danielwells <danielwells.usnret@gmail.com>
2014-04-04 13:58:25 -0500

    New rails app

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Ex0404BankAjax::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ex0404BankAjax::Application.config.secret_token = 'f33aa453ec99ebc1c79bf137692b02feab624127dbe142976af38ab1a1ad8ce8603a1449c5f967f8d62711ae10ad191ed0c27f3ca0f37b16fe0188c32095d73e'
